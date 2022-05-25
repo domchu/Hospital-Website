992c69aa640b95b81241dbeff3812897aa3c1055
Mr Dominic <ochu.dominic.onah@gmail.com>
2022-05-25 23:34:30 +0100

    added  animation and responsive

---
+++ app.js
@@ -0,0 +1,12 @@
+let hamburger =document.querySelector(".ham");
+let unlist =document.querySelector(".unlist");
+let  times = document.querySelector(".times");
+
+hamburger.addEventListener("click", () =>{
+    unlist.style.display = "block";
+});
+times.addEventListener("click", () =>{
+    unlist.style.display ="none"
+})
+
+console.log("am working");
